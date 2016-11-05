d19d2e4c676b2558096eb87d38893133394ba82a
Seb <webmarken@gmail.com>
2016-11-05 21:27:09 +0530

    updated frontend

--- public/js/app.js
+++ public/js/app.js
@@ -2,22 +2,26 @@ var name = getQueryVariable('name') || 'Anonymous';
 var room = getQueryVariable('room');
 var socket = io();
 
-socket.on('connect', function(){
+socket.on('connect', function() {
 	console.log('connected to socket.io server');
 	console.log(name + ' joined ' + room);
 });
 
-socket.on('message', function(msg){
+socket.on('message', function(msg) {
 	var momentTimestamp = moment.utc(msg.timestamp);
 	var $message = jQuery('.messages');
 
-	$message.append('<div class="card"><div class="card-block"><h4 class="card-title">' + msg.text + '</h4><p class="card-text">' + momentTimestamp.local().format('HH:mm') + ' ' + msg.name + '</p></div></div>');
+	if (msg.name === name){
+		$message.append('<div class="row"><div class="col-sm-10"><div class="card card-outline-primary text-xs-left"><div class="card-block"><p class="card-text">' + msg.text + '</div><div class="card-footer text-muted">' + momentTimestamp.local().format('HH:mm') + ' ' + msg.name + '</div></div></div><div class="col-sm-2"></div></div>');
+	} else {
+		$message.append('<div class="row"><div class="col-sm-2"></div><div class="col-sm-10"><div class="card card-outline-success text-xs-right"><div class="card-block"><p class="card-text">' + msg.text + '</div><div class="card-footer text-muted">' + momentTimestamp.local().format('HH:mm') + ' ' + msg.name + '</div></div></div></div>');
+	}
 });
 
 // Handles submitting of new msg
 var $form = jQuery('#msg-form');
 
-$form.on('submit', function(event){
+$form.on('submit', function(event) {
 	event.preventDefault();
 	var $message = $form.find('input[name=message]');
 
@@ -27,5 +31,5 @@ $form.on('submit', function(event){
 	})
 
 	$message.val('');
-	
+
 });
