e79176fb86f9e037ff63b443bd9ac8c09c464781
Seb <webmarken@gmail.com>
2016-11-05 17:25:49 +0530

    changed index

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,7 @@ socket.on('connect', function(){
 
 socket.on('message', function(msg){
 	var momentTimestamp = moment.utc(msg.timestamp);
-	jQuery('.messages').append('<p class="right"><strong>' + momentTimestamp.local().format('HH:mm') + ':</strong> ' + msg.text + '</p>');
+	jQuery('.messages').append('<p><strong>' + momentTimestamp.local().format('HH:mm') + ':</strong> ' + msg.text + '</p>');
 });
 
 // Handles submitting of new msg
@@ -15,8 +15,6 @@ var $form = jQuery('#msg-form');
 $form.on('submit', function(event){
 	event.preventDefault();
 	var $message = $form.find('input[name=message]');
-
-	// jQuery('.messages').append('<p class="left">Me: ' + $message.val() + '</p>');
 
 	socket.emit('message', {
 		text: $message.val()
